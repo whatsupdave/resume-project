5fae4d5133fdbac4ae5420d4ab920e5ca88e8426
Eivydas K <kantautaseivydas@gmail.com>
2025-07-08 13:45:58 +0300

    added get_lat_lon_for_city() func

--- airflow/dags/weather_api.py
+++ airflow/dags/weather_api.py
@@ -3,6 +3,40 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime, timedelta
 
+import requests
+from airflow.hooks.base import BaseHook
+
+
+def get_lat_lon_for_city(city: str = "Vilnius") -> tuple[float, float]:
+    """
+    Gets latitude and longitude for the specified city.
+
+    Args:
+        city: City name to get coordinates for (default: "Vilnius")
+
+    Returns:
+        tuple[float, float]: Latitude and longitude coordinates
+
+    Raises:
+        ValueError: If no location is found for the specified city
+    """
+    conn = BaseHook.get_connection(f"openweathermap_default")
+    api_key = conn.password
+    endpoint = (
+        f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={api_key}"
+    )
+
+    response = requests.get(endpoint).json()
+
+    if not response:
+        raise ValueError(f"No location found for {city}")
+
+    lat = response[0]["lat"]
+    lon = response[0]["lon"]
+
+    return lat, lon
+
+
 # DAG Config
 DAG_NAME = "weather_monitor"
 DAG_DESCRIPTION = "Gets weather data"
