d4648a84771f8e7fd8394d9f07688591b79c76ce
Eivydas K <kantautaseivydas@gmail.com>
2025-08-07 11:37:44 +0300

    adjusted test folder logic+gitignore

--- airflow/tests/test_weather_api.py
+++ airflow/tests/test_weather_api.py
@@ -1,1 +1,7 @@
-from airflow.dags.weather_api import transform_weather_data
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'dags'))
+from weather_api import transform_weather_data
+
+def test_transform_weather_data():
+    print()
